cfe634912bd33fb725f294b5681548b922bd0463
diez-dominguez <david.diez-dominguez@adesso.de>
2021-11-18 17:42:19 +0100

    take a picture with webcam and save it in firebase storage

--- js/db.js
+++ js/db.js
@@ -3,7 +3,11 @@ import { initializeApp } from "firebase/app";
 import {
   getFirestore, collection, getDocs,
   addDoc, deleteDoc, doc, GeoPoint
-} from 'firebase/firestore'
+} from 'firebase/firestore';
+
+import {
+  getStorage, ref, getDownloadURL, uploadString
+} from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCAN0iXjvVJ4WqZkYVGFUvGN1uGWgY94es",
@@ -15,7 +19,8 @@ const firebaseConfig = {
 };
 
 //init app
-initializeApp(firebaseConfig)
+const firebaseApp = initializeApp(firebaseConfig);
+const storage = getStorage(firebaseApp);
 
 const db = getFirestore()
 
@@ -87,4 +92,74 @@ deletePhotoForm.addEventListener('submit', (e) => {
       deletePhotoForm.reset()
     })
 })
-console.log();
+
+
+function uploadImage() {
+  const ref = storage().ref();
+  const file = document.querySelector("#photo").files[0];
+  const name = +new Date() + "-" + file.name;
+  const metadata = {
+    contentType: file.type
+  };
+  const task = ref.child(name).put(file, metadata);
+  task
+    .then(snapshot => snapshot.ref.getDownloadURL())
+    .then(url => {
+      console.log(url);
+      document.querySelector("#image").src = url;
+    })
+    .catch(console.error);
+}
+
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const snap = document.getElementById("snap");
+const errorMsgElement = document.querySelector('span#errorMsg');
+
+const constraints = {
+  audio: false,
+  video: {
+    width: 400, height: 400
+  }
+};
+
+// Access webcam
+async function init() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
+}
+
+// Success
+function handleSuccess(stream) {
+  window.stream = stream;
+  video.srcObject = stream;
+}
+
+// Load init
+init();
+
+// Draw image
+var context = canvas.getContext('2d');
+snap.addEventListener("click", function () {
+  context.drawImage(video, 0, 0, 640, 480);
+  var image = new Image();
+  image.id = "pic";
+  image.src = canvas.toDataURL();
+  console.log(image.src)
+  var button = document.createElement('button')
+  button.textContent = 'Upload Image'
+  document.body.appendChild(button)
+
+  button.addEventListener('click', () => {
+    const storageRef = ref(storage, "penis");
+    uploadString(storageRef, image.src, "data_url").then((snapshot) => {
+      console.log(snapshot);
+    });
+  })
+
+}
+);
